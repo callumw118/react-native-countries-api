1c06ca918cc6d2ce3405447228e2fbb56059e7b2
Callum Wolfe <wolfe.callum@gmail.com>
2020-10-27 11:43:25 +0000

    display country by region with drop down option

--- App.js
+++ App.js
@@ -1,21 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, FlatList, TouchableWithoutFeedback, Alert, TextInput } from 'react-native';
+import {Picker} from '@react-native-picker/picker';
 
 export default function App() {
+
+  const [countries, setCountries] = useState([]);
+  const [selectedRegion, setSelectedRegion] = useState("Americas")
+
+  function fetchCountries() {
+    fetch(`https://restcountries.eu/rest/v2/region/${selectedRegion}?fields=name`)
+      .then(res => res.json())
+      .then(data => setCountries(data))
+  }
+
+  function handleCountryClick(country) {
+    Alert.alert(country.name)
+  }
+
+  // Empty array here is to stop this from running when the countries data updates
+  // Can pass a value into the array which will trigger useEffect again when
+  // that state is updated
+  useEffect(() => {
+    fetchCountries();
+  }, [selectedRegion])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <Picker style={styles.container}
+        selectedValue={selectedRegion}
+        onValueChange={(itemValue, itemIndex) => setSelectedRegion(itemValue)}
+      >
+        <Picker.Item label="Americas" value="Americas" />
+        <Picker.Item label="Asia" value="Asia" />
+        <Picker.Item label="Europe" value="Europe" />
+        <Picker.Item label="Africa" value="Africa" />
+      </Picker>
+      <FlatList
+        data={countries}
+        keyExtractor={item => item.name} //key
+        renderItem={({ item }) => {
+          return (
+            <TouchableWithoutFeedback
+              onPress={() => handleCountryClick(item)}
+            >
+              <View>
+                <Text style={styles.text}>{item.name}</Text>
+              </View>
+            </TouchableWithoutFeedback>
+          )
+        }}
+      />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 30,
+    paddingTop: 30
   },
+  text: {
+    fontSize: 20,
+    margin: 10
+  }
 });
